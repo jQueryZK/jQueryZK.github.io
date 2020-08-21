26cdef68807fe5a5b9d87f67be850ae587f27aae
jQueryZK <2472353424@qq.com>
2020-08-21 15:28:41 +0800

    Site updated: 2020-08-21 15:28:41

--- js/Lete.js
+++ js/Lete.js
@@ -75,5 +75,5 @@ function qipao() {
 qipao();
 //页脚博主
 $(document).ready(function(e) {
-    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i>');
+    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> jQueryZK');
 });
