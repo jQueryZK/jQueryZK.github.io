dc1f938d2ef63a965991299584719fbe063524d5
jQueryZK <2472353424@qq.com>
2020-08-22 14:09:23 +0800

    Site updated: 2020-08-22 14:09:22

--- js/Lete.js
+++ js/Lete.js
@@ -1,15 +1,3 @@
-/*添加图片top*/
-var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
-/*添加到返回顶部按钮下*/
-document.getElementById("go-up").innerHTML += top_up;
-/* 手机客户端导航栏默认隐藏 */
-var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
-var menus_item_child = mobile_sidebar_menus.getElementsByClassName("menus_item_child");
-var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
-for (var i = 0; i < menus_item_child.length; i++) {
-    menus_item_child[i].style.display = "none";
-    menus_expand[i].className += " closed";
-}
 // 可爱的Title
 var OriginTitle = document.title;
 var titleTime;
@@ -26,56 +14,7 @@ document.addEventListener('visibilitychange', function() {
         }, 2000);
     }
 });
-// 气泡
-function qipao() {
-    $('#page-header').circleMagic({
-        radius: 10,
-        density: .2,
-        color: 'rgba(255,255,255,.4)',
-        clearOffset: 0.99
-    });
-}! function(p) {
-    p.fn.circleMagic = function(t) {
-        var o, a, n, r, e = !0,
-            i = [],
-            d = p.extend({ color: "rgba(255,0,0,.5)", radius: 10, density: .3, clearOffset: .2 }, t),
-            l = this[0];
-
-        function c() { e = !(document.body.scrollTop > a) }
-
-        function s() { o = l.clientWidth, a = l.clientHeight, l.height = a + "px", n.width = o, n.height = a }
-
-        function h() {
-            if (e)
-                for (var t in r.clearRect(0, 0, o, a), i) i[t].draw();
-            requestAnimationFrame(h)
-        }
-
-        function f() {
-            var t = this;
-
-            function e() { t.pos.x = Math.random() * o, t.pos.y = a + 100 * Math.random(), t.alpha = .1 + Math.random() * d.clearOffset, t.scale = .1 + .3 * Math.random(), t.speed = Math.random(), "random" === d.color ? t.color = "rgba(" + Math.floor(255 * Math.random()) + ", " + Math.floor(0 * Math.random()) + ", " + Math.floor(0 * Math.random()) + ", " + Math.random().toPrecision(2) + ")" : t.color = d.color }
-            t.pos = {}, e(), this.draw = function() { t.alpha <= 0 && e(), t.pos.y -= t.speed, t.alpha -= 5e-4, r.beginPath(), r.arc(t.pos.x, t.pos.y, t.scale * d.radius, 0, 2 * Math.PI, !1), r.fillStyle = t.color, r.fill(), r.closePath() }
-        }! function() {
-            o = l.offsetWidth, a = l.offsetHeight,
-                function() {
-                    var t = document.createElement("canvas");
-                    t.id = "canvas", t.style.top = 0, t.style.zIndex = 0, t.style.position = "absolute", l.appendChild(t), t.parentElement.style.overflow = "hidden"
-                }(), (n = document.getElementById("canvas")).width = o, n.height = a, r = n.getContext("2d");
-            for (var t = 0; t < o * d.density; t++) {
-                var e = new f;
-                i.push(e)
-            }
-            h()
-        }(), window.addEventListener("scroll", c, !1), window.addEventListener("resize", s, !1)
-    }
-}(jQuery);
-
-// 调用气泡方法
-qipao();
 //页脚博主
 $(document).ready(function(e) {
     $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> jQueryZK');
 });
-//鼠标爱心
-!function(e,t,a){function n(){c(".heart{width: 10px;height: 10px;position: fixed;background: #f00;transform: rotate(45deg);-webkit-transform: rotate(45deg);-moz-transform: rotate(45deg);}.heart:after,.heart:before{content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;-webkit-border-radius: 500%;-moz-border-radius: 50%;position: fixed;}.heart:after{top: -5px;}.heart:before{left: -5px;}"),o(),r()}function r(){for(var e=0;e<d.length;e++)d[e].alpha<=0?(t.body.removeChild(d[e].el),d.splice(e,1)):(d[e].y--,d[e].scale+=.004,d[e].alpha-=.013,d[e].el.style.cssText="left:"+d[e].x+"px;top:"+d[e].y+"px;opacity:"+d[e].alpha+";transform:scale("+d[e].scale+","+d[e].scale+") rotate(45deg);background:"+d[e].color+";z-index:99999");requestAnimationFrame(r)}function o(){var t="function"==typeof e.onclick&&e.onclick;e.onclick=function(e){t&&t(),i(e)}}function i(e){var a=t.createElement("div");a.className="heart",d.push({el:a,x:e.clientX-5,y:e.clientY-5,scale:1,alpha:1,color:s()}),t.body.appendChild(a)}function c(e){var a=t.createElement("style");a.type="text/css";try{a.appendChild(t.createTextNode(e))}catch(t){a.styleSheet.cssText=e}t.getElementsByTagName("head")[0].appendChild(a)}function s(){return"rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"}var d=[];e.requestAnimationFrame=function(){return e.requestAnimationFrame||e.webkitRequestAnimationFrame||e.mozRequestAnimationFrame||e.oRequestAnimationFrame||e.msRequestAnimationFrame||function(e){setTimeout(e,1e3/60)}}(),n()}(window,document);
